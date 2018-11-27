022dcc420dae27a7a5c6ed87602c668f0a48acdc
Kitzing <sara.kitzing@hotmail.com>
2018-11-27 23:38:01 +0100

    CREATED Mission-parts of the controller. SK

---
+++ src/mdsd/server/controller/Mission.java
@@ -0,0 +1,18 @@
+package mdsd.server.controller;
+
+import com.sun.xml.internal.bind.v2.TODO;
+
+import java.awt.*;
+import java.util.List;
+
+public class Mission {
+
+    private List<Point> points;
+
+    //TODO change type after askning about it in the supervision
+    private int constraints;
+
+    public void updateMission() {
+
+    }
+}
