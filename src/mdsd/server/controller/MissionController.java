4fe23b9213fec0c97991a777172e3fde7269b45d
Kitzing <sara.kitzing@hotmail.com>
2018-11-27 23:38:01 +0100

    CREATED Mission-parts of the controller. SK

---
+++ src/mdsd/server/controller/MissionController.java
@@ -0,0 +1,22 @@
+package mdsd.server.controller;
+
+public class MissionController {
+
+
+    public Mission createNewMission() {
+
+        return null;
+    }
+
+    public void updateMission() {
+
+    }
+
+    public void supplyStrategy() {
+
+    }
+
+    public void sendMission() {
+
+    }
+}
