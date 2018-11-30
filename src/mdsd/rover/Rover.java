423801d91391e3d8264248303713deb6d4e394d5
sk8eddies <sk8eddie97@gmail.com>
2018-11-30 14:03:37 +0100

    IMPLEMENTED the methods in RoverNetwork. EM

--- src/mdsd/rover/Rover.java
+++ src/mdsd/rover/Rover.java
@@ -1,14 +1,21 @@
 package mdsd.rover;
 
+import mdsd.Robot;
 import project.Point;
 
 import java.util.List;
 
-public class Rover {
+public class Rover extends Robot {
+
+    public Rover(Point startPosition, String name){
+        super(startPosition, name);
+    }
 
     private Point currentDestination;
     private List<Point> gPSCoordinates;
     private RoverNetwork roverNetwork;
+
+
 
     private void setRoverDestination () {
 
