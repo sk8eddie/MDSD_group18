fb15ea29aa431747657c981c5fab0662a6cd7e01
sk8eddies <sk8eddie97@gmail.com>
2018-11-30 14:03:37 +0100

    IMPLEMENTED the methods in RoverNetwork. EM

--- src/mdsd/rover/RoverNetwork.java
+++ src/mdsd/rover/RoverNetwork.java
@@ -7,15 +7,24 @@ import simbad.sim.CameraSensor;
 public class RoverNetwork implements RoverCommunication {
 
     private ServerInterface server;
+    private Rover rover;
 
-
-
-    public void setNewDestination(Point newDestination) {
-
+    public RoverNetwork(ServerInterface server, Rover rover){
+        this.server = server;
+        this.rover = rover;
     }
 
-    public Point getDestination() {
-        return null;
+    public void setNewDestination(Point newDestination) {
+        this.rover.setDestination(newDestination);
+    }
+
+    public Point getPosition() {
+        return rover.getPosition();
+    }
+
+    // TODO Should be the camera feed, not a boolean, just need to figure out how to access the camera class
+    public Boolean getCamera(){
+        return rover.checkCameraDetection();
     }
 
 }
