d48472ae5cab1bbc2c0270f3e2d705413eec90d5
sk8eddies <sk8eddie97@gmail.com>
2018-11-30 14:33:23 +0100

    IMPLEMENTED Rover class. EM

--- src/mdsd/rover/Rover.java
+++ src/mdsd/rover/Rover.java
@@ -5,23 +5,32 @@ import project.Point;
 
 import java.util.List;
 
+/**
+ * The Rover is a robot roaming around the environment. It is not very "smart" but will follow instructions
+ * from a controller component.
+ */
 public class Rover extends Robot {
 
+    /**
+     * Calls cuntructor of its superclass.
+     * @param startPosition
+     * @param name
+     */
     public Rover(Point startPosition, String name){
         super(startPosition, name);
     }
 
     private Point currentDestination;
     private List<Point> gPSCoordinates;
-    private RoverNetwork roverNetwork;
 
-
-
-    private void setRoverDestination () {
-
+    void setRoverDestination(Point newDestination) {
+        this.setDestination(newDestination);
     }
 
+    /**
+     * Should verify if there are any physical faults with the Rover, broken sensors etc.
+     */
     private void checkRoverFault () {
-
+        // TODO way to surveil the rover parts
     }
 }
