d490005d8691af7d1664c7eba7258a1ee05fbb4c
Tuubaas <tobli@student.chalmers.se>
2018-12-04 14:15:25 +0100

    Added logicalarea class

---
+++ src/mdsd/server/model/LogicalArea.java
@@ -0,0 +1,20 @@
+package mdsd.server.model;
+
+import java.awt.*;
+
+public class LogicalArea implements Area{
+    @Override
+    public Area createArea(int areaNumber, float position) {
+        return null;
+    }
+
+    @Override
+    public String getAreaType() {
+        return null;
+    }
+
+    @Override
+    public boolean inArea(Point coordinate) {
+        return false;
+    }
+}
