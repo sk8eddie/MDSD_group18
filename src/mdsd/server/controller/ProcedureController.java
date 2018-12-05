16fac42a940e9a644a138c2deaf38927ef204945
Kitzing <sara.kitzing@hotmail.com>
2018-12-05 11:17:09 +0100

    Implemented setCurrentProcedure.SK

--- src/mdsd/server/controller/ProcedureController.java
+++ src/mdsd/server/controller/ProcedureController.java
@@ -1,21 +1,74 @@
 package mdsd.server.controller;
 
-import java.sql.Time;
+import mdsd.Robot;
+import mdsd.server.model.ServerModel;
+import org.omg.CORBA.Environment;
 
-/**
- * Created by Eddie on 2018-11-27.
- */
+import java.sql.Time;
+import java.time.Clock;
+import java.time.Duration;
+import java.util.Iterator;
+import java.util.Set;
+
+
 public class ProcedureController {
 
     public enum Procedure {
         A, B;
     }
 
-    private Time clock;
-    private Procedure currentProcedure;
+    //private Clock clock;
+    private int clock = 20;
+    private Procedure currentProcedure = Procedure.A;
+    private Environment environment;
 
 
-    public void computeRewardPoints() {
+    private void computeRewardPoints(Set<Robot> robots) {
+        if (clock % 20 == 0) {
+            setCurrentProcedure(robots);
+
+            switch (currentProcedure) {
+                case A:
+                    procedureA();
+                    break;
+                case B:
+                    procedureB();
+                    break;
+            }
+
+        }
+
 
     }
+
+    private void procedureA() {
+
+    }
+
+    private void procedureB() {
+
+    }
+
+    private void setCurrentProcedure(Set<Robot> robots) {
+        Iterator<Robot> iterator = robots.iterator();
+
+        while (iterator.hasNext()){
+            Robot r = iterator.next();
+            if(currentProcedure == Procedure.A){
+              //  if(environment.getRoverArea(r.getPosition()).getAreaType().equals("Logical")){
+              //      currentProcedure = Procedure.B;
+               // }
+            }else if(currentProcedure == Procedure.B){
+              //  if(environment.getRoverArea(r.getPosition()).getAreaType().equals("Physical")){
+              //      currentProcedure = Procedure.A;
+              //  }
+            }
+        }
+
+
+
+    }
+
+
+
 }
