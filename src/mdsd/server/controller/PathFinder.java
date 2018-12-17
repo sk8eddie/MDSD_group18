be9e906034bd10df6c9cd6118f16e08b6fa29c8b
Andrius Sakalas <gussakan@student.gu.se>
2018-12-17 13:18:35 +0100

    added PathFinding

--- src/mdsd/server/controller/PathFinder.java
+++ src/mdsd/server/controller/PathFinder.java
@@ -1,10 +1,18 @@
 package mdsd.server.controller;
 
+import mdsd.GridEnvironment;
+import mdsd.server.model.Area;
+import mdsd.server.model.Environment;
+
 import org.xguzm.pathfinding.grid.GridCell;
 import org.xguzm.pathfinding.grid.NavigationGrid;
 import org.xguzm.pathfinding.grid.finders.AStarGridFinder;
 import org.xguzm.pathfinding.grid.finders.GridFinderOptions;
 
+import project.Point;
+import simbad.sim.AbstractWall;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class PathFinder {
@@ -15,18 +23,38 @@ public class PathFinder {
 	 * Read the XML and points send to PathFinder
 	 * New list of points sent to MissionController
 	 * Mission controller send to Rover
-	 *
+	 * 
 	 */
-
-	public List<GridCell> getFastestPath(NavigationGrid<GridCell> navGrid){
-
+	
+	GridEnvironment grid = new GridEnvironment(20, 20);
+	Environment env;
+	
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public List<GridCell> getFastestPath(Point start, Point end){
+		
+		grid.createCells(env.wallList());
+		
 		GridFinderOptions opt = new GridFinderOptions();
 		opt.allowDiagonal = false;
 		AStarGridFinder<GridCell> finder = new AStarGridFinder(GridCell.class, opt);
 
-		List<GridCell> pathToEnd = finder.findPath(0, 0, 20, 20, navGrid);
-
+		List<GridCell> pathToEnd = finder.findPath((int) start.getX(), (int) start.getZ(), (int)end.getZ(), (int)end.getX(), grid.createNavGrid());
+		
 		return pathToEnd;
+	}
+	
+	public Mission getPathPoints(Point start, Point end){
+		
+        Mission mission = new Mission();
+		List<GridCell> gridPoints = new ArrayList<GridCell>();
+		
+		gridPoints = getFastestPath(start,end);
+		
+		for (GridCell a : gridPoints){
+           Point newPoint = new Point(a.getX(),a.getY());
+           mission.addPoint(newPoint);
+        }
+		return mission;
 	}
 
 }
