1d46cb1246c6f2071570c286dfa56349b065f46d
Tuubaas <tobli@student.chalmers.se>
2018-12-17 13:25:27 +0100

    Comments Area

--- src/mdsd/server/model/Area.java
+++ src/mdsd/server/model/Area.java
@@ -6,10 +6,22 @@ import simbad.sim.AbstractWall;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Interface for Areas.
+ */
 public interface Area {
 
+    /**
+     * Getter for AreaType.
+     * @return areaType.
+     */
     String getAreaType();
 
+    /**
+     * Method for checking if a coordinate is in an Area.
+     * @param coordinate
+     * @return
+     */
     boolean inArea(Point coordinate);
 
     String getAreaName();
