f3cc768f7ba86ea8c5676c41cd8e3ac1085392d1
Kitzing <sara.kitzing@hotmail.com>
2019-01-03 13:24:28 +0100

    added comments to Mission

--- src/mdsd/server/controller/Mission.java
+++ src/mdsd/server/controller/Mission.java
@@ -8,56 +8,86 @@ import java.util.Iterator;
 import java.util.List;
 
 
-public class Mission implements Iterable{
+public class Mission implements Iterable {
 
-	private Point currentDestination;
+    private Point currentDestination;
     private List<Point> points;
     private Iterator missionIterator = new MissionIterator();
 
-    public Mission(){
-    	this.points = new ArrayList<>();
-	}
+    /**
+     * Empty constructor for the Mission, creates an empty arraylist for the points
+     */
+    public Mission() {
+        this.points = new ArrayList<>();
+    }
 
-	public Mission(List<Point> points){
-    	this.points = points;
-	}
 
-	public void addPoint(Point point){
-		this.points.add(point);
-	}
+    /**
+     * Constructor for the Mission, sets the list of points to the paramteters list of points
+     *
+     * @param points list of mission points
+     */
+    public Mission(List<Point> points) {
+        this.points = points;
+    }
 
-	public List<Point> getPoints(){
-		return this.points;
-	}
+    /**
+     * Adds a new point to the points-list
+     *
+     * @param point the point to be added
+     */
+    public void addPoint(Point point) {
+        this.points.add(point);
+    }
 
-	public Point getCurrentDestination(){
-		return this.currentDestination;
-	}
+    /**
+     * Getter for the points-list
+     *
+     * @return a list of points
+     */
+    public List<Point> getPoints() {
+        return this.points;
+    }
 
-   public void updateStrategy(){ //TODO handle for when a rover has reached a certain amount of points.
-	   Collections.shuffle(this.points);
-   }
+    /**
+     * Getter for the current destination for the mission
+     *
+     * @return the missions current destination
+     */
+    public Point getCurrentDestination() {
+        return this.currentDestination;
+    }
 
-	@Override
-	public Iterator iterator() {
-		return this.missionIterator;
-	}
+    /**
+     * Updates the strategy by shuffling the the mission-points
+     */
+    public void updateStrategy() { //TODO handle for when a rover has reached a certain amount of points.
+        Collections.shuffle(this.points);
+    }
 
-	private class MissionIterator implements Iterator<Point>{
-   		private int currentIndex = 0;
 
-		@Override
-		public boolean hasNext() {
-		    return currentIndex < points.size();
-			//return points.get(currentIndex) != null;
-		}
+    @Override
+    public Iterator iterator() {
+        return this.missionIterator;
+    }
 
-		@Override
-		public Point next() {
-			Point nextPoint = points.get(currentIndex);
-			currentDestination = nextPoint;
-			currentIndex++;
-			return nextPoint;
-		}
-	}
+    /**
+     * An iterator for the mission
+     */
+    private class MissionIterator implements Iterator<Point> {
+        private int currentIndex = 0;
+
+        @Override
+        public boolean hasNext() {
+            return currentIndex < points.size();
+        }
+
+        @Override
+        public Point next() {
+            Point nextPoint = points.get(currentIndex);
+            currentDestination = nextPoint;
+            currentIndex++;
+            return nextPoint;
+        }
+    }
 }
