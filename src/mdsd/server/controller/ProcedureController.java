1ed4a71621ebb44b1a0438d311ba9d1046a502f1
Kitzing <sara.kitzing@hotmail.com>
2019-01-03 12:31:10 +0100

    added comments to and refactored ProcedureController

--- src/mdsd/server/controller/ProcedureController.java
+++ src/mdsd/server/controller/ProcedureController.java
@@ -10,6 +10,9 @@ import java.util.*;
 
 public class ProcedureController extends TimerTask {
 
+    /**
+     * The different procedures that can be chosen
+     */
     public enum Procedure {
         A, B;
     }
@@ -21,114 +24,131 @@ public class ProcedureController extends TimerTask {
     private Map<Area, Integer> envpnt = new HashMap<Area, Integer>();
 
 
-    public ProcedureController(Set<RoverCommunication> rovers, Environment env, Map<Area, Integer> envpnt, ServerModel model){
-        this.model = model;
+    /**
+     * Constructor for the ProcedureController
+     *
+     * @param rovers list of the rovers in the environment
+     * @param env    the Environment
+     * @param envpnt the areas of the environment as well as corresponding points of each of the areas
+     * @param model  the ServerModel
+     */
+    public ProcedureController(Set<RoverCommunication> rovers, Environment env, Map<Area, Integer> envpnt, ServerModel model) {
         this.rovers = rovers;
         this.env = env;
         this.envpnt = envpnt;
+        this.model = model;
     }
 
 
-
-
-    // Calculates the reward points depending on which procedure should be used,
-    // Overrided from TimerTask
+    /**
+     * Override from TimerTask
+     * Loops through all rovers in the environment when the current procedure is sat
+     * Calculates the reward points gained from each rover depending on which procedure should be used
+     */
     @Override
     public void run() {
         setCurrentProcedure();
         Iterator<RoverCommunication> iterator = rovers.iterator();
-            RoverCommunication rover;
+        RoverCommunication rover;
 
 
         while (iterator.hasNext()) {
             switch (currentProcedure) {
                 case A:
-                        rover = iterator.next();
-                        procedureA(rover);
+                    rover = iterator.next();
+                    procedure(rover, "Physical");
                     break;
                 case B:
-                        rover = iterator.next();
-                        procedureB(rover);
+                    rover = iterator.next();
+                    procedure(rover, "Logical");
                     break;
             }
         }
 
 
-
-
     }
 
-    //Calculates reward points with procedure A
-    private void procedureA(RoverCommunication rover) {
-        for(Map.Entry<Area, Integer> values : envpnt.entrySet()){
-            if (values.getKey().getAreaType().equals("Physical")){
-                if(values.getKey().inArea(rover.getPosition())){
-                    model.setRewardPoints(model.getRewardPoints() + values.getValue());
-                }
-            }
-        }
-
-
-     /*   if(env.getRoverArea(rover.getPosition()).getAreaName().equals("office")){
-            model.setRewardPoints(model.getRewardPoints() + 1);
-        }
-
-        else if(env.getRoverArea(rover.getPosition()).getAreaName().equals("teachingRoom")){
-            model.setRewardPoints(model.getRewardPoints() + 2);
-        }
-    */
-    }
-
-    //Calculates reward points with procedure B
-    private void procedureB(RoverCommunication rover) {
+    /**
+     * Calculates reward points with procedure
+     * Done by checking if the rover is located in any of the areas
+     * rewarded by the currentProcedure (physical when A, logical when B)
+     * and add the reward points connected to that area if the rover is located in it
+     *
+     * @param rover the rover to be checked if within an rewarded area
+     */
+    private void procedure(RoverCommunication rover, String areaType) {
         for (Map.Entry<Area, Integer> values : envpnt.entrySet()) {
-            if (values.getKey().getAreaType().equals("Logical")) {
+            if (values.getKey().getAreaType().equals(areaType)) {
                 if (values.getKey().inArea(rover.getPosition())) {
                     model.setRewardPoints(model.getRewardPoints() + values.getValue());
                 }
             }
-
-      /*  if(env.getRoverArea(rover.getPosition()).getAreaName().equals("wifiZone")){
-            model.setRewardPoints(model.getRewardPoints() + 1);
-        }
-        else if(env.getRoverArea(rover.getPosition()).getAreaName().equals("eatingArea")){
-            model.setRewardPoints(model.getRewardPoints() + 2);
-
-        }
-        */
-
         }
     }
 
-    //Sets current procedure depending on which area type the rovers are in
+    /**
+     * Calculates reward points with procedure A
+     * Done by checking if the rover is located in any of the physical areas
+     * and add the reward points connected to that area if the rover is located in it
+     *
+     * @param rover the rover to be checked if within an rewarded area
+     */
+    /**
+     private void procedureA(RoverCommunication rover) {
+     for (Map.Entry<Area, Integer> values : envpnt.entrySet()) {
+     if (values.getKey().getAreaType().equals("Physical")) {
+     if (values.getKey().inArea(rover.getPosition())) {
+     model.setRewardPoints(model.getRewardPoints() + values.getValue());
+     }
+     }
+     }
+     }
+     */
+    /**
+     * Calculates reward points with procedure B
+     * Done by checking if the rover is located in any of the logical areas
+     * and add the reward points connected to that area if the rover is located in it
+     *
+     * @param rover the rover to be checked if within an rewarded area
+     */
+/**
+ private void procedureB(RoverCommunication rover) {
+ for (Map.Entry<Area, Integer> values : envpnt.entrySet()) {
+ if (values.getKey().getAreaType().equals("Logical")) {
+ if (values.getKey().inArea(rover.getPosition())) {
+ model.setRewardPoints(model.getRewardPoints() + values.getValue());
+ }
+ }
+ }
+ }
+ */
+
+    /**
+     * Sets the current procedure depending on which area type the rovers are in
+     * <p>
+     * If the current procedure is A and at least one rover is in a logical area
+     * the procedure should change to B.
+     * <p>
+     * If the current procedure is B and at least one rover is in a physical area
+     * the procedure should change to A.
+     */
     private void setCurrentProcedure() {
         Iterator<RoverCommunication> iterator = rovers.iterator();
 
-        while (iterator.hasNext()){
+        while (iterator.hasNext()) {
             RoverCommunication rover = iterator.next();
 
-            if(currentProcedure == Procedure.A){
-                System.out.println("---------- setProcedure if A ----------" );
-               if(env.getRoverArea(rover.getPosition()).getAreaType().equals("Logical")){
-                   currentProcedure = Procedure.B;
-                   System.out.println("---------- setProcedure in A ----------" );
-                   System.out.println("----------" + currentProcedure +"----------" );
-                   break;
+            if (currentProcedure == Procedure.A) {
+                if (env.getRoverArea(rover.getPosition()).getAreaType().equals("Logical")) {
+                    currentProcedure = Procedure.B;
+                    break;
                 }
-            }else {
-                System.out.println("---------- setProcedure if B ----------" );
-                if(env.getRoverArea(rover.getPosition()).getAreaType().equals("Physical")){
+            } else {
+                if (env.getRoverArea(rover.getPosition()).getAreaType().equals("Physical")) {
                     currentProcedure = Procedure.A;
-                    System.out.println("---------- setProcedure in B ----------" );
                     break;
                 }
             }
         }
-
-
-
     }
-
-
-
 }
