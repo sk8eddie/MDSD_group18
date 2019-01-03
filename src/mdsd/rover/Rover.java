32960dacff5c05d2cd6669e18a64b0703e22ca8e
Kitzing <sara.kitzing@hotmail.com>
2019-01-03 12:48:49 +0100

    added comments to Rover

--- src/mdsd/rover/Rover.java
+++ src/mdsd/rover/Rover.java
@@ -18,48 +18,87 @@ public class Rover extends Robot {
     private Chassi roverChassi;
 
     /**
-     * Calls cuntructor of its superclass and adds a chassi.
-     * @param startPosition
-     * @param name
+     * Rover constructor, calls cuntructor of its superclass and adds a chassi.
+     *
+     * @param startPosition the starting position for the rover
+     * @param name          the name of the rover
+     * @param chassi        the chassi to be added
      */
-    public Rover(Point startPosition, String name, Chassi chassi){
+    public Rover(Point startPosition, String name, Chassi chassi) {
         super(startPosition, name);
         this.roverChassi = chassi;
     }
 
-    public Rover(Point startPosition, String name){
+    /**
+     * Rover constructor, calls cuntructor of its superclass.
+     *
+     * @param startPosition the starting position for the rover
+     * @param name          the name of the rover
+     */
+    public Rover(Point startPosition, String name) {
         super(startPosition, name);
         this.roverChassi = new RegularChassi();
     }
 
+    /**
+     * Stops the rover by setting the its destination to the current position
+     */
     void stopRover() {
         System.out.println("STOP, THE ROVER IS BROKEN!");
         this.currentDestination = this.getPosition();
         this.setRoverDestination(this.currentDestination);
     }
 
-    void hitChassi () {
+    void hitChassi() {
         if (!roverChassi.sustainHit())
             this.stopRover();
     }
 
-    boolean isAtDestination(){
+    /**
+     * Checks if the rover is at its destination
+     *
+     * @return True if the destination is reached, otherwise false
+     */
+    boolean isAtDestination() {
         return (isinXRange(this.getPosition().getX()) && isinZRange(this.getPosition().getZ()));
     }
 
-    private Boolean isinXRange(double x){
+    /**
+     * Checks if the parameter x is in the same position as the currentDestinations x-parameter
+     *
+     * @param x the parameter to be checked
+     * @return True if x is in the range of the currentDestinations x-parameter, otherwise false
+     */
+    private Boolean isinXRange(double x) {
         return (((this.currentDestination.getX() - 0.5) < x) && (x < (this.currentDestination.getX() + 0.5)));
     }
 
-    private Boolean isinZRange(double z){
+    /**
+     * Checks if the parameter z is in the same position as the currentDestinations z-parameter
+     *
+     * @param z the parameter to be checked
+     * @return True if z is in the range of the currentDestinations z-parameter, otherwise false
+     */
+    private Boolean isinZRange(double z) {
         return (((this.currentDestination.getZ() - 0.5) < z) && (z < (this.currentDestination.getZ() + 0.5)));
     }
 
+    /**
+     * Sets the rovers current destination to the newDestination
+     * Calls the setDestination-method from the AbstractRobotSimulator-class
+     *
+     * @param newDestination the new destination for the rover
+     */
     void setRoverDestination(Point newDestination) {
         this.currentDestination = newDestination;
         this.setDestination(newDestination);
     }
 
+    /**
+     * getter for the rovers current destination
+     *
+     * @return the rovers current destination
+     */
     public Point getRoverDestination() {
         return currentDestination;
     }
@@ -67,7 +106,7 @@ public class Rover extends Robot {
     /**
      * Should verify if there are any physical faults with the Rover, broken sensors etc.
      */
-    private void checkRoverFault () {
+    private void checkRoverFault() {
         // TODO way to surveil the rover parts
         // Poll att the parts???
     }
